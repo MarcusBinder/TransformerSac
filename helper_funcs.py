6597fc1ac2de3d8939c856f602d9bc1003c77c29
Marcus Binder <marcusbinder2@gmail.com>
2026-02-18 15:12:32 +0100

    Make basic config

--- helper_funcs.py
+++ helper_funcs.py
@@ -721,6 +721,60 @@ ENV_CONFIGS: Dict[str, Dict[str, Any]] = {
             "ws_min": 10, "ws_max": 14,
         },  
     },
+
+    "basic": {
+        "yaw_init": "Random",
+        "BaseController": "Local",
+        "ActionMethod": "yaw",
+        "Track_power": False,
+        "farm": {"yaw_min": -30, "yaw_max": 30},
+        "wind": {
+            "ws_min": 10, "ws_max": 10,
+            "TI_min": 0.07, "TI_max": 0.07,
+            "wd_min": 260, "wd_max": 280,
+        },
+        "act_pen": {"action_penalty": 0.0, "action_penalty_type": "Change"},
+        "power_def": {"Power_reward": "Power_avg", "Power_avg": 1, "Power_scaling": 1.0},
+        "mes_level": {
+            "turb_ws": True,
+            "turb_wd": True,
+            "turb_TI": False,
+            "turb_power": True,
+            "farm_ws": False,
+            "farm_wd": False,
+            "farm_TI": False,
+            "farm_power": False,
+        },
+        "ws_mes": {
+            "ws_current": True,
+            "ws_rolling_mean": False,
+            "ws_history_N": 1,
+            "ws_history_length": 1,
+            "ws_window_length": 1,
+        },
+        "wd_mes": {
+            "wd_current": True,
+            "wd_rolling_mean": False,
+            "wd_history_N": 15,
+            "wd_history_length": 15,
+            "wd_window_length": 1,
+        },
+        "yaw_mes": {
+            "yaw_current": True,
+            "yaw_rolling_mean": False,
+            "yaw_history_N": 1,
+            "yaw_history_length": 1,
+            "yaw_window_length": 1,
+        },
+        "power_mes": {
+            "power_current": True,
+            "power_rolling_mean": False,
+            "power_history_N": 1,
+            "power_history_length": 1,
+            "power_window_length": 1,
+        },
+    },
+
     "wide": {
         "wind": {
             "wd_min": 250, "wd_max": 290, 
