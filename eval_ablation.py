069e42b53d02f27b4d1575db90094686a7aa327b
Marcus Binder <marcusbinder2@gmail.com>
2026-05-22 08:37:59 +0100

    Switch eval to E6

--- eval_ablation.py
+++ eval_ablation.py
@@ -524,7 +524,7 @@ if __name__ == "__main__":
     DETERMINISTIC = True
     VERBOSE = True
 
-    EVAL_LAYOUT = "E4"
+    EVAL_LAYOUT = "E6"
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
